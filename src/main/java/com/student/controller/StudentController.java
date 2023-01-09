f4ab7a627af7ad5df070f11b06fad9a4be131590
virendrapratappatel <121235787+virendrapratappatel@users.noreply.github.com>
2023-01-09 10:06:30 +0530

    Update StudentController.java

--- src/main/java/com/student/controller/StudentController.java
+++ src/main/java/com/student/controller/StudentController.java
@@ -38,8 +38,8 @@ public class StudentController {
 	@GetMapping
 		//@Cacheable("student")
 	public List<StudentModel> getAllStudentDetails() {
-		//return this.studentService.getAllStudent();
-		return null;
+		return this.studentService.getAllStudent();
+		//return null;
 	}
 
 	@GetMapping("getById")
